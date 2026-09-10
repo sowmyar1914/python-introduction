3033308a883f85b9ac87f7cd0acbdee15d6f3ab5
sowmyar1914 <136739186+sowmyar1914@users.noreply.github.com>
2026-09-10 13:51:02 -0500

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,21 @@
+print("Hello") 
+# my personal introduction
+
+print("=" * 40)  # Prints a line of 40 equal signs
+print("student profile")
+print("=" * 40)
+# personal details stored as variables 
+first_name = "Sowmya"
+last_name = "Ragi"
+age = 30
+city = "Minnesota"
+previous_occupation = "Game Quality Analyst" # from 2021-2023 i worked in velan studios 
+# Display the introduction 
+print("Name:" , first_name, last_name)
+print("Age:" , age)
+print("City:" , city)
+print("previous_occupation:" , previous_occupation) 
+print("Why I'm learning to code:")
+print("I want to transition into a software engineering role.\nAlso i love problem solving and building things from scratch.")
+print("fun fact: I have a passion for cooking and trying out new recipes.")
+print("=" * 40)
